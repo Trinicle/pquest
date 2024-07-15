70c36f92c3e2396bf8c4e6d27cf3471f8fb14a77
Hunter Freeland <hunter.freeland@parkplacetech.com>
2024-07-15 17:04:07 -0400

    daisyui and fixed textg box

--- src/app/quests/create-quest/create-quest.component.ts
+++ src/app/quests/create-quest/create-quest.component.ts
@@ -1,27 +1,24 @@
-import { Component, computed, OnDestroy, OnInit, signal } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Dialog, DialogModule } from '@angular/cdk/dialog';
 import { ModalComponent } from './modal/modal.component';
 
 @Component({
   selector: 'pq-create-quest',
   standalone: true,
-  imports: [CommonModule, DialogModule],
+  imports: [CommonModule, ModalComponent],
   templateUrl: './create-quest.component.html',
   styleUrls: ['./create-quest.component.scss'],
 })
 export class CreateQuestComponent implements OnInit, OnDestroy {
-  constructor(private dialog: Dialog) {}
+  @ViewChild(ModalComponent) modalComponent!: ModalComponent;
+
+  constructor() {}
   ngOnInit(): void {}
   ngOnDestroy(): void {
     throw new Error('Method not implemented.');
   }
 
-  onToggleCreateQuest() {
-    const dialogRef = this.dialog.open<string>(ModalComponent, {
-      width: '700px',
-      panelClass: 'create-quest-dialog',
-    });
-    dialogRef.closed.subscribe();
+  openModal() {
+    this.modalComponent.showModal();
   }
 }
--- src/app/quests/create-quest/modal/modal.component.ts
+++ src/app/quests/create-quest/modal/modal.component.ts
@@ -1,52 +1,17 @@
-import {
-  Component,
-  computed,
-  inject,
-  Inject,
-  model,
-  OnDestroy,
-  OnInit,
-  signal,
-} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {
-  FormGroup,
-  FormControl,
-  Validators,
-  ReactiveFormsModule,
-  FormsModule,
-} from '@angular/forms';
-import { DIALOG_DATA, DialogModule, DialogRef } from '@angular/cdk/dialog';
-import { LiveAnnouncer } from '@angular/cdk/a11y';
-
-import { COMMA, ENTER } from '@angular/cdk/keycodes';
-import { ChangeDetectionStrategy } from '@angular/core';
-import {
-  MatAutocompleteModule,
-  MatAutocompleteSelectedEvent,
-} from '@angular/material/autocomplete';
-import { MatChipInputEvent, MatChipsModule } from '@angular/material/chips';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'pq-modal',
   standalone: true,
-  imports: [
-    CommonModule,
-    ReactiveFormsModule,
-    DialogModule,
-    MatFormFieldModule,
-    MatChipsModule,
-    MatIconModule,
-    MatAutocompleteModule,
-    FormsModule,
-  ],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.scss'],
+  styleUrl: './modal.component.scss',
 })
 export class ModalComponent implements OnInit, OnDestroy {
+  @ViewChild('modal') modal: any;
+
   form = new FormGroup({
     title: new FormControl('', [
       Validators.required,
@@ -72,60 +37,21 @@ export class ModalComponent implements OnInit, OnDestroy {
     ]),
     repository: new FormControl(''),
   });
-  readonly separatorKeysCodes: number[] = [ENTER, COMMA];
 
-  allKeys: string[] = [
-    'Angular',
-    'React',
-    'Azure',
-    'HTML',
-    'CSS',
-    'TypeScript',
-    'JavaScript',
-    'C#',
-    '.NET',
-  ];
+  constructor() {}
 
-  constructor(
-    private dialogRef: DialogRef<string>,
-    @Inject(DIALOG_DATA) private data: any,
-  ) {}
-
-  ngOnInit(): void {}
-  ngOnDestroy(): void {}
-
-  toggleSubmit() {
-    this.dialogRef.close();
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  ngOnDestroy(): void {
+    throw new Error('Method not implemented.');
   }
 
-  readonly keys = signal([] as string[]);
-  readonly filteredKeys = computed(() => {
-    const currentKey = this.form.get('currentKey')?.value?.toLowerCase();
-    return currentKey
-      ? this.allKeys.filter(key => key.toLowerCase().includes(currentKey))
-      : this.allKeys.slice();
-  });
-
-  readonly announcer = inject(LiveAnnouncer);
-
-  remove(fruit: string): void {
-    this.keys.update(keys => {
-      const index = keys.indexOf(fruit);
-      if (index < 0) {
-        return keys;
-      }
-
-      keys.splice(index, 1);
-      this.announcer.announce(`Removed ${fruit}`);
-      return [...keys];
-    });
+  showModal() {
+    this.modal.nativeElement.showModal();
   }
 
-  selected(event: MatAutocompleteSelectedEvent): void {
-    this.keys.update(keys => [...keys, event.option.viewValue]);
-
-    this.form.patchValue({ currentKey: '' });
-    console.log(this.form.get('currentKey')?.value);
-    event.option.deselect();
+  closeModal() {
+    this.modal.nativeElement.close();
   }
 }
